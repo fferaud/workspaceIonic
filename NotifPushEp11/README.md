67db9ad843cc9fc02913f09a794bba6291de4cd9
fferaud <fabferaud@orange.fr>
2018-10-15 00:24:13 +0200

    feat(NotifPushEp11): add notification push project

---
+++ NotifPushEp11/README.md
@@ -0,0 +1,14 @@
+# Projet Notification Push
+
+* Use of OneSignal website to send push notifications
+* Create OneSignal.com account
+* Get firebase key for our project
+* Set it in our oneSignal project
+* Create categories to setup notifications
+* Send notifications immediately or set date and hour
+
+# Notes
+Only on real devices
+
+# Application screenshot
+<img src="./src/assets/imgs/screen.jpg" height="500" />
---
+++ NotifPushEp11/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+
+// Native Components
+import { OneSignal } from '@ionic-native/onesignal';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private oneSignal: OneSignal) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      this.oneSignal.startInit('eb628570-88bd-4d23-8fe0-d4878454b231', '541960967930');
+
+      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+      this.oneSignal.handleNotificationReceived().subscribe(() => {
+      // do something when notification is received
+      });
+
+      this.oneSignal.handleNotificationOpened().subscribe(() => {
+        // do something when a notification is opened
+      });
+
+      this.oneSignal.endInit();
+    });
+  }
+}
+
